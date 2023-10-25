5536370e23845eb366d0bccd13a082554a65c2fc
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2023-10-25 16:50:47 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello")  // we are using nodjs, not browser
+
+console.log(3+3)
+
+//code readability should be high
+console.log("Sachin")
+
+let name="Sachin"
+let age=23
+let isLoggedIn= false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone values  // object
+// undefined =>   // undefined console print
+// symbol => unique ness
+
+    //OBJECT
+
+console.log(typeof "sachin");
+
