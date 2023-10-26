116d793442ebdb8111efd7921f66297bca2b24b8
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2023-10-26 11:39:08 +0000

    datesinJS

---
+++ 01_Basics/07_datesinJS.js
@@ -0,0 +1,23 @@
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocalString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,3)
+let myCreatedDate = new Date("2023-01-14")
+console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
