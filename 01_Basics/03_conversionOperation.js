882bd535f460166ad4858bd4b05fcd75a167ed4b
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-22 13:45:32 +0000

    container config done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-// let score = "33abc"
+let score = "33abc"
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // // "33" => 33
 // // "33abc" = > NaN
@@ -38,26 +38,26 @@
 // console.log(2*2);
 
 
-// let str1 = "hello"
-// let str2 = " sachin"
+ let str1 = "hello"
+ let str2 = " sachin"
 
-// let str3 = str1 + str2
-// console.log(str3);
+ let str3 = str1 + str2
+ console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
-// console.log(3 + 4 * 5 % 3);
+ console.log(3 + 4 * 5 % 3);
 
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
+// let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+// num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
-++gameConunter;
-console.log(gameCounter);
+// let gameCounter = 100
+// ++gameConunter;
+// console.log(gameCounter);
