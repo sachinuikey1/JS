1ad6ca050e8f571eec2382929f6a2321dabd666b
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-22 14:27:24 +0000

    container

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accounEmail = "uikeysachin4@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gondia"
+
+// accountId = 2  // not allowed
+
+// {
+
+      // cirlly braces are the scope.
+// }
+
+accountEmail = "sac@ui.com"
+accountPassword =  "21212121"
+accountCity = "Nagpur"
+
+console.log(accountId);
+
+console.table([accountId, accounEmail, accountPassword, accountCity])
